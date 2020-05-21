e5cd6f7e31058c3542e20f00f1c31144500dc72b
sorrow132 <50179475+sorrow132@users.noreply.github.com>
2020-05-21 12:58:45 +0300

    Initial commit

---
+++ app/src/main/java/yuresko/diffutilkotlincleanproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package yuresko.diffutilkotlincleanproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import yuresko.diffutil_kotlin_clean_project.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/yuresko/diffutilkotlincleanproject/adapter/RecyclerView.kt
@@ -0,0 +1,4 @@
+package yuresko.diffutilkotlincleanproject.adapter
+
+class RecyclerView() {
+}
